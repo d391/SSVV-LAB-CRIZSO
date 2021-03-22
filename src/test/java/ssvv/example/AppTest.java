da5f8f0252eed82d03b11ed02f66938f0e405bc1
Dezso Noemi <dezsonn@gmail.com>
2021-03-22 13:52:45 +0200

    smol fix

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -56,6 +56,7 @@ public class AppTest
         try
         {
             ctrl.add(params);
+            fail();
         }
         catch (IllegalArgumentException | ValidatorException ex)
         {
