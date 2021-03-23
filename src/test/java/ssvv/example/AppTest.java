60d8955cc10f7ed84f52cd52daa637e1dc509f33
Andre Cristescu <andrecrisss@yahoo.com>
2021-03-23 12:47:11 +0200

    added condition coverage bla bla

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -3,8 +3,11 @@ package ssvv.example;
 import org.junit.Test;
 import ssvv.example.Exceptions.ValidatorException;
 import ssvv.example.Repository.XMLFileRepository.StudentXMLRepo;
+import ssvv.example.Repository.XMLFileRepository.TemaLabXMLRepo;
 import ssvv.example.Service.XMLFileService.StudentXMLService;
+import ssvv.example.Service.XMLFileService.TemaLabXMLService;
 import ssvv.example.Validator.StudentValidator;
+import ssvv.example.Validator.TemaLabValidator;
 
 import static org.junit.Assert.*;
 
@@ -19,6 +22,10 @@ public class AppTest
     private StudentXMLRepo repo;
     private StudentValidator validator;
     private StudentXMLService ctrl;
+
+    private TemaLabXMLRepo tema_repo;
+    private TemaLabValidator tema_validator;
+    private TemaLabXMLService tema_ctrl;
 
     @Test
     public void shouldAnswerWithTrue()
@@ -64,5 +71,39 @@ public class AppTest
             System.out.println(ex.getMessage());
         }
 
+    }
+
+    @Test
+    public void tc_validHomework() {
+        tema_validator = new TemaLabValidator();
+        tema_repo = new TemaLabXMLRepo(tema_validator, "ValidTemaTest.xml");
+        tema_ctrl = new TemaLabXMLService(tema_repo);
+        String[] params = {"1","abcd","12","7"};
+        try {
+            tema_ctrl.add(params);
+        } catch (ValidatorException ex)
+        {
+            fail();
+            System.out.println(ex.getMessage());
+        }
+        assertEquals(1, tema_ctrl.getSize());
+    }
+
+    @Test
+    public void tc_invalidHomework() {
+        tema_validator = new TemaLabValidator();
+        tema_repo = new TemaLabXMLRepo(tema_validator, "ValidTemaTest.xml");
+        tema_ctrl = new TemaLabXMLService(tema_repo);
+        String[] params = {"","abcde","12","8"};
+        try
+        {
+            tema_ctrl.add(params);
+            fail();
+        }
+        catch (IllegalArgumentException | ValidatorException ex)
+        {
+            assertTrue(true);
+            System.out.println(ex.getMessage());
+        }
     }
 }
